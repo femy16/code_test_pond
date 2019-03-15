419ce4dbbf7a35377689a18c1932ccc169074b82
Femy Stephen <femystephen16@gmail.com>
2019-03-15 22:41:31 +0000

    preparing for heroku 1

--- code_test.py
+++ code_test.py
@@ -77,4 +77,4 @@ def media_details(id):
         return("Id out of range")
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8081, debug=True)
+	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
